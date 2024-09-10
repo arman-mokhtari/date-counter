0ffb38c84b1e418b34719db7fd81811178bab9ab
Arman Mokhtari <rmn.mokhtarii@gmail.com>
2024-09-10 16:13:59 +0330

    text edited

--- src/components/StartScreen.js
+++ src/components/StartScreen.js
@@ -3,7 +3,7 @@ import React from "react";
 const StartScreen = ({ numQuestions, dispatch }) => {
   return (
     <div className="start">
-      <h2>Welcome to the React Quiz App</h2>
+      <h2>Welcome to the Quiz App</h2>
       <h3>{numQuestions} questions to test your knowledge</h3>
       <button
         className="btn btn-ui"

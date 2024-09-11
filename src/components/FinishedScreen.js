a6f7becbbb1223d6815020d62246f54520ca1299
Arman Mokhtari <rmn.mokhtarii@gmail.com>
2024-09-11 08:46:52 +0330

    timer implemented

--- src/components/FinishedScreen.js
+++ src/components/FinishedScreen.js
@@ -1,15 +1,15 @@
 import React from "react";
 
-const FinishedScreen = ({ points, maxPossiblePoints, highscore }) => {
+const FinishedScreen = ({ points, maxPossiblePoints, highscore,dispatch }) => {
   const percentage = (points / maxPossiblePoints) * 100;
 
   let emoji;
 
   if (percentage === 100) emoji = "🥇";
-  if (percentage >= 80 && percentage === 100) emoji = "🎉";
-  if (percentage >= 50 && percentage === 80) emoji = "😄";
-  if (percentage >= 0 && percentage === 50) emoji = "🤔";
-  if (percentage === 0) emoji = "🤔";
+  else if (percentage >= 80) emoji = "🎉"; 
+  else if (percentage >= 50) emoji = "😄"; 
+  else if (percentage > 0) emoji = "🤔";
+  else emoji = "😢";
 
   return (
     <>
@@ -18,6 +18,12 @@ const FinishedScreen = ({ points, maxPossiblePoints, highscore }) => {
         {maxPossiblePoints} ({Math.ceil(percentage)}%)
       </p>
       <p className="highscore">(highscore: {highscore} points)</p>
+      <button
+          className="btn btn-ui"
+          onClick={() => dispatch({ type: "restart" })}
+        >
+          Restart Quiz
+        </button>
     </>
   );
 };
